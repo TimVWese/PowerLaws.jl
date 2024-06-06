ab230c6dd0b2b89605b43df54b4658bea0936147
Tim Van Wesemael <tim.vanwesemael@ugent.be>
2024-06-06 10:49:57 +0200

    Deploty documentation

--- docs/make.jl
+++ docs/make.jl
@@ -15,6 +15,8 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/TimVWese/PowerLaws.jl",
+    versions = ["stable" => "v^", "v#.#"],
+)
+
